01a40a134868547f6c3a56cd39bd040d1f26328d
Elton Okawa <elton.okawa@gmail.com>
2023-05-28 08:42:30 -0300

    feat: add graphql endpoint

---
+++ packages/blog-app/app/api/graphql/route.ts
@@ -0,0 +1,22 @@
+import { createYoga, createSchema } from 'graphql-yoga'
+ 
+const { handleRequest } = createYoga({
+  schema: createSchema({
+    typeDefs: /* GraphQL */ `
+      type Query {
+        greetings: String
+      }
+    `,
+    resolvers: {
+      Query: {
+        greetings: () =>
+          'This is the `greetings` field of the root `Query` type'
+      }
+    }
+  }),
+ 
+  graphqlEndpoint: '/api/graphql',
+  fetchAPI: { Response }
+})
+ 
+export { handleRequest as GET, handleRequest as POST }
