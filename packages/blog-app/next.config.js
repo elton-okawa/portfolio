ecf5784869a186e40ded15b2c9a3ccf2cfee2199
Elton Okawa <elton.okawa@gmail.com>
2023-06-04 18:19:24 -0300

    feat: add analyze tool

--- packages/blog-app/next.config.js
+++ packages/blog-app/next.config.js
@@ -20,4 +20,7 @@ const nextConfig = {
   pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
 };
 
-module.exports = nextConfig;
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+module.exports = withBundleAnalyzer(nextConfig);
