ed02620de6b37cb32b976dc09704dc7abb1e5a1f
Elton Okawa <elton.okawa@gmail.com>
2023-06-18 17:33:13 -0300

    feat: add linter packages

---
+++ packages/eslint-config-react/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: ["react", "@typescript-eslint"],
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier", // must be the last one
+  ],
+  parser: "@typescript-eslint/parser",
+  rules: {},
+};
