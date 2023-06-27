272bee4f5f313676630edb006542803f71cd539e
Elton Okawa <elton.okawa@gmail.com>
2023-06-27 20:42:58 -0300

    feat: add navbar

---
+++ apps/web/src/lib/shared/NavBar.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+export function NavBar() {
+  return (
+    <div className="inline-flex px-5 mt-2 bg-base-100 rounded-full shadow-[0_0_4px_1px_rgba(0,0,0,0.1)]">
+      <NavLink path="/" name="Home" />
+      <NavLink path="/articles" name="Articles" />
+    </div>
+  );
+}
+
+type NavLinkProps = {
+  path: string;
+  name: string;
+};
+
+function NavLink({ path, name }: NavLinkProps) {
+  const router = useRouter();
+  const active = router.pathname === path;
+
+  return (
+    <Link
+      href={path}
+      className={`p-2 hover:text-secondary ${
+        active && 'text-primary border-b-2'
+      }`}
+    >
+      {name}
+    </Link>
+  );
+}
