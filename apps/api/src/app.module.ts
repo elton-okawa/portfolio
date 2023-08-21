abf7869a401180195a8349e92922328e01ba4b34
Elton Okawa <elton.okawa@gmail.com>
2023-08-21 14:41:59 -0300

    feat: add simple hello query

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { YogaDriver, YogaDriverConfig } from '@graphql-yoga/nestjs';
+import { join } from 'path';
+import { HelloModule } from './hello/hello.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    GraphQLModule.forRoot<YogaDriverConfig>({
+      driver: YogaDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    HelloModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
