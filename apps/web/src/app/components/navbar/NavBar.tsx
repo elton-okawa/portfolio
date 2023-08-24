e4b6f956dae91f1f3cc819e4a20b8c5a1964cd86
Elton Okawa <elton.okawa@gmail.com>
2023-08-24 17:06:46 -0300

    feat: sticky navbar

--- apps/web/src/app/components/navbar/NavBar.tsx
+++ apps/web/src/app/components/navbar/NavBar.tsx
@@ -7,17 +7,19 @@ import { NavLink } from './NavLink';
 
 export function NavBar() {
   return (
-    <div className="mt-2 flex items-center justify-between">
-      <div className="w-32 flex align-center">
-        <Logo />
+    <header className="py-2 fixed top-0 w-full bg-gray-50 dark:bg-gray-700 shadow-sm">
+      <div className="flex items-center justify-between max-w-screen-md mx-auto">
+        <div className="w-32 flex align-center">
+          <Logo />
+        </div>
+        <nav className="bg-default inline-flex px-5 bg-base-100 rounded-full shadow-[0_0_4px_1px_rgba(0,0,0,0.1)]">
+          <NavLink path="/" name="Home" />
+          <NavLink path="/articles" name="Articles" />
+        </nav>
+        <div className="w-32 flex justify-end">
+          <ThemeButton />
+        </div>
       </div>
-      <div className="bg-default inline-flex px-5 bg-base-100 rounded-full shadow-[0_0_4px_1px_rgba(0,0,0,0.1)]">
-        <NavLink path="/" name="Home" />
-        <NavLink path="/articles" name="Articles" />
-      </div>
-      <div className="w-32 flex justify-end">
-        <ThemeButton />
-      </div>
-    </div>
+    </header>
   );
 }
