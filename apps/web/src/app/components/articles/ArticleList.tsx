8091e4ef179e18dba9e57f1b5d5f4412b5414cf2
Elton Okawa <elton.okawa@gmail.com>
2023-08-25 09:24:49 -0300

    feat: article list title

--- apps/web/src/app/components/articles/ArticleList.tsx
+++ apps/web/src/app/components/articles/ArticleList.tsx
@@ -24,14 +24,14 @@ export async function ArticleList() {
 
   return (
     <div className="flex flex-col gap-2">
+      <h3 className="text-lg text-accent font-bold mt-5">Recently Published</h3>
       {data.posts.map((post) => (
         <ArticleSummary
           key={post.id}
           {...post}
           // TODO add on api
           slug="placeholder"
-          photoUrl="/placeholder.jpg"
-          description="placeholder description"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum scelerisque placerat urna, vitae luctus lectus. Suspendisse id eros at elit suscipit vulputate quis sed sapien. In id tellus varius, gravida nibh sed, faucibus felis. Mauris eleifend hendrerit feugiat. Cras at aliquet tortor. Maecenas orci ligula, cursus ac "
         />
       ))}
     </div>
