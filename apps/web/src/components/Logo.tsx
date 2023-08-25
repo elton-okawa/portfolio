1e8562b13dfad3df967f06dc6bc893926dfa39c7
Elton Okawa <elton.okawa@gmail.com>
2023-08-25 08:34:10 -0300

    fix: wrap logo in a div to avoid growing more

--- apps/web/src/components/Logo.tsx
+++ apps/web/src/components/Logo.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 
 export function Logo() {
   return (
-    <Link
-      href="/"
-      className="inline-block animate-typing overflow-hidden whitespace-nowrap font-mono font-semibold text-xl text-primary tracking-tight border-b-2 border-transparent hover:border-secondary"
-    >
-      Elton Okawa
-    </Link>
+    <div>
+      <Link
+        href="/"
+        className="inline-block animate-typing overflow-hidden whitespace-nowrap font-mono font-semibold text-xl text-primary tracking-tight border-b-2 border-transparent hover:border-secondary"
+      >
+        Elton Okawa
+      </Link>
+    </div>
   );
 }
