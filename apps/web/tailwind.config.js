a2cb81fe50381cca30500c4941952ecf68f70784
Elton Okawa <elton.okawa@gmail.com>
2023-08-25 11:08:44 -0300

    feat: use tailwind plugin to set theme colors effortlessly

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,24 +1,38 @@
 const colors = require('tailwindcss/colors');
+const { createThemes } = require('tw-colors');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
-  plugins: [],
   darkMode: 'class',
+  plugins: [
+    createThemes(
+      {
+        light: {
+          primary: colors.indigo['500'],
+          secondary: colors.orange['400'],
+          accent: colors.teal['600'],
+        },
+        dark: {
+          primary: colors.indigo['300'],
+          secondary: colors.orange['300'],
+          accent: colors.teal['500'],
+        },
+      },
+      { getThemeClassName: (themeName) => themeName }
+    ),
+  ],
   theme: {
     extend: {
-      colors: {
-        primary: colors.indigo['500'],
-        secondary: colors.orange['400'],
-        accent: colors.teal['600'],
-      },
       keyframes: ({ theme }) => ({
         typing: {
           from: { width: '0%' },
           to: { width: '100%' },
         },
         'blink-caret': {
-          from: { borderRight: `0.2em solid ${theme('colors.secondary')}` },
+          from: {
+            borderRight: `0.2em solid ${theme('colors.secondary')}`,
+          },
           to: { borderColor: theme('colors.secondary') },
           '50%': { borderColor: 'transparent' },
         },
