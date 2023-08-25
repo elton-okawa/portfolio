e7105461767fd1f1bdbf9439a3ae3cf8bd63eedb
Elton Okawa <elton.okawa@gmail.com>
2023-08-25 09:29:06 -0300

    fix: footer container width

--- apps/web/src/app/components/footer/Footer.tsx
+++ apps/web/src/app/components/footer/Footer.tsx
@@ -3,13 +3,17 @@ import { Typography, Logo } from '@/components';
 
 export function Footer() {
   return (
-    <footer className="flex justify-between p-5 bg-indigo-50 dark:bg-gray-800">
-      <div className="flex flex-col items-start">
-        <Logo />
-        <Typography variant="description">2022-present Elton Okawa</Typography>
+    <footer className="bg-indigo-50 dark:bg-gray-800">
+      <div className="flex justify-between max-w-screen-md py-5 mx-auto">
+        <div className="flex flex-col items-start">
+          <Logo />
+          <Typography variant="description">
+            2022-present Elton Okawa
+          </Typography>
+        </div>
+        {/* TODO add links */}
+        <div />
       </div>
-      {/* TODO add links */}
-      <div />
     </footer>
   );
 }
