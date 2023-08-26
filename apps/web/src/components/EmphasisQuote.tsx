b9c8d06f60f9c351d9c5a32c67b2859d0b418175
Elton Okawa <elton.okawa@gmail.com>
2023-08-26 20:07:15 -0300

    fix: adjust dark theme on newer elements

--- apps/web/src/components/EmphasisQuote.tsx
+++ apps/web/src/components/EmphasisQuote.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export function EmphasisQuote({ text }: { text: string }) {
-  return <p className="italic text-lg text-center p-5 bg-gray-200">{text}</p>;
+  return (
+    <p className="italic text-lg text-center p-5 bg-gray-200 dark:bg-gray-800">
+      {text}
+    </p>
+  );
 }
