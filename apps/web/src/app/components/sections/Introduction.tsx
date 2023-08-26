feff31e5ad5ce158dcd35b5b9b31a56f80ada946
Elton Okawa <elton.okawa@gmail.com>
2023-08-26 16:42:43 -0300

    feat: style role

--- apps/web/src/app/components/sections/Introduction.tsx
+++ apps/web/src/app/components/sections/Introduction.tsx
@@ -3,23 +3,24 @@ import React from 'react';
 export function Introduction() {
   return (
     <div className="w-full h-full flex flex-col items-start justify-center gap-1">
-      <p>{"Hi, I'm"}</p>
+      <p className="m-0">{"Hi, I'm"}</p>
       <span>
-        <p className="text-4xl text-primary font-semibold overflow-hidden whitespace-nowrap font-mono animate-[typing_1.5s_steps(18,end),blink-caret_1s_step-end_infinite]">
+        <p className="text-4xl text-primary font-bold overflow-hidden whitespace-nowrap font-mono animate-[typing_1.5s_steps(18,end),blink-caret_1s_step-end_infinite]">
           Elton Yoshio Okawa
         </p>
       </span>
-      <span>
-        <p className="text-2xl animate-fade-up">
-          Full Stack Software Developer with
-        </p>
-        <p className="text-2xl opacity-0 animate-fade-up animate-delay-1000">
-          <span className="text-accent before:content-[counter(count)] before:animate-[counter_1s_linear_forwards] before:animate-delay-1000">
-            + years
-          </span>{' '}
-          of working experience
-        </p>
-      </span>
+      <p className="text-2xl m-0 animate-fade-up">
+        <span className="text-secondary font-semibold">
+          Full Stack Software Developer
+        </span>{' '}
+        with
+      </p>
+      <p className="text-2xl m-0 opacity-0 animate-fade-up animate-delay-1000">
+        <span className="text-accent before:content-[counter(count)] before:animate-[counter_1s_linear_forwards] before:animate-delay-1000">
+          + years
+        </span>{' '}
+        of working experience
+      </p>
       <br />
       <p className="opacity-0 animate-fade animate-delay-2000">
         My values:{' '}
