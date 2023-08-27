194d7c1c298574489d9266f51f4b3954d5b626b1
Elton Okawa <elton.okawa@gmail.com>
2023-08-27 09:05:03 -0300

    feat: add easter egg

---
+++ apps/web/src/app/components/easter-egg/EasterEgg.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+export function EasterEgg() {
+  console.log(
+    `==============================================
+  
+Pretty curious, huh? 🧐
+
+Checkout the GitHub code:
+https://github.com/elton-okawa/portfolio
+
+==============================================`
+  );
+
+  return null;
+}
