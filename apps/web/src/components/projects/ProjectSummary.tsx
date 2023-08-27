d620da09abd3baa367455558ad0fd4db0bb1724f
Elton Okawa <elton.okawa@gmail.com>
2023-08-27 17:01:13 -0300

    feat: style project summary

--- apps/web/src/components/projects/ProjectSummary.tsx
+++ apps/web/src/components/projects/ProjectSummary.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 type ProjectSummaryProps = {
   slug: string;
@@ -11,17 +12,22 @@ type ProjectSummaryProps = {
 
 export function ProjectSummary(props: ProjectSummaryProps) {
   return (
-    <div className="w-80">
-      <div className="relative w-full h-48">
-        <Image
-          src={props.imagePath}
-          alt={props.slug}
-          fill
-          className="object-cover"
-        />
-      </div>
-      <p className="font-bold">{props.title}</p>
-      <p className="text-description">{props.description}</p>
+    <div className="w-80 hover:text-secondary hover:shadow-md">
+      <Link href={`projects/${props.slug}`}>
+        <div className="relative w-full h-48 mb-2">
+          <Image
+            src={props.imagePath}
+            alt={props.slug}
+            fill
+            className="object-cover"
+          />
+        </div>
+        <div className="px-5 py-2">
+          <p className="font-bold text-inherit text-lg">{props.title}</p>
+          <p className="text-description">{props.description}</p>
+          <p className="text-default font-semibold">View project</p>
+        </div>
+      </Link>
     </div>
   );
 }
