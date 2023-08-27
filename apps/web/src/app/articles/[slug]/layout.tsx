05544b7b1c31e2c45f733989e6a63636014aa9d4
Elton Okawa <elton.okawa@gmail.com>
2023-08-27 14:27:05 -0300

    fix: use article api

--- apps/web/src/app/articles/[slug]/layout.tsx
+++ apps/web/src/app/articles/[slug]/layout.tsx
@@ -4,18 +4,25 @@ import { getClient } from '@/lib/apollo/apollo-ssr-client';
 import { toShortDate } from '@/lib/date';
 
 const query = gql(/* GraphQL */ `
-  query Post($id: String!) {
-    post(id: $id) {
-      id
+  query Article($slug: String!) {
+    article(slug: $slug) {
       title
+      description
+      updatedAt
     }
   }
 `);
 
-export default async function Layout({ children }: React.PropsWithChildren) {
-  const { data } = await getClient().query({
+type LayoutProps = React.PropsWithChildren<{
+  params: { slug: string };
+}>;
+
+export default async function Layout({ children, params }: LayoutProps) {
+  const {
+    data: { article },
+  } = await getClient().query({
     query,
-    variables: { id: '64e4832cdf9b82fc4637f672' }, // TODO use url slug
+    variables: { slug: params.slug },
     context: {
       fetchOptions: {
         next: { revalidate: 3600 },
@@ -23,20 +30,14 @@ export default async function Layout({ children }: React.PropsWithChildren) {
     },
   });
 
-  // TODO add data on api
-  const post = {
-    ...data.post,
-    description:
-      'sem eget dignissim euismod, mauris nisl commodo ex, sit amet dignissim purus sem at metus. Sed accumsan semper magna eget tempus. Nunc tincidunt turpis sed tellus pellentesque, id pulvinar quam tempor',
-    updatedAt: new Date(),
-  };
-
   return (
     <div className="max-w-screen-sm mx-auto mt-16 px-5 ">
       <div className="my-2">
-        <h1>{post.title}</h1>
-        <p className="text-description">{toShortDate(post.updatedAt)}</p>
-        <p className="text-description">{post.description}</p>
+        <h1>{article.title}</h1>
+        <p className="text-description">
+          {toShortDate(new Date(article.updatedAt))}
+        </p>
+        <p className="text-description">{article.description}</p>
       </div>
       <div className="bg-default px-5 py-2">{children}</div>
     </div>
--- apps/web/src/app/articles/components/articles/ArticleList.tsx
+++ apps/web/src/app/articles/components/articles/ArticleList.tsx
@@ -4,10 +4,11 @@ import { getClient } from '@/lib/apollo/apollo-ssr-client';
 import { gql } from '@/__generated__/gql';
 
 const query = gql(/* GraphQL */ `
-  query Posts {
-    posts {
-      id
+  query Articles {
+    articles {
+      slug
       title
+      description
     }
   }
 `);
@@ -23,17 +24,11 @@ export async function ArticleList() {
   });
 
   return (
-    <div className="flex flex-col gap-2">
+    <>
       <h3 className="text-lg text-accent font-bold mt-5">Recently Published</h3>
-      {data.posts.map((post) => (
-        <ArticleSummary
-          key={post.id}
-          {...post}
-          // TODO add on api
-          slug="placeholder"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum scelerisque placerat urna, vitae luctus lectus. Suspendisse id eros at elit suscipit vulputate quis sed sapien. In id tellus varius, gravida nibh sed, faucibus felis. Mauris eleifend hendrerit feugiat. Cras at aliquet tortor. Maecenas orci ligula, cursus ac "
-        />
+      {data.articles.map((article) => (
+        <ArticleSummary key={article.slug} {...article} />
       ))}
-    </div>
+    </>
   );
 }
