5899d1280f466aaab2b8392ea8efba346d3b1a53
Elton Okawa <elton.okawa@gmail.com>
2023-09-02 14:56:26 -0300

    feat: add title on home page

--- apps/web/src/app/components/sections/Projects.tsx
+++ apps/web/src/app/components/sections/Projects.tsx
@@ -1,9 +1,10 @@
-import { ProjectList } from '@/components';
+import { AccentTitle, ProjectList } from '@/components';
 import React from 'react';
 
 export function Projects() {
   return (
     <div className="max-w-screen-md mx-auto">
+      <AccentTitle>Projects</AccentTitle>
       <ProjectList showcase />
     </div>
   );
