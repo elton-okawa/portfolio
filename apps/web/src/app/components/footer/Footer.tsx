b54eaaee6c8d6a340b1cdba0072f26cff92c536d
Elton Okawa <elton.okawa@gmail.com>
2024-06-30 07:19:36 -0300

    feat: adjust footer

--- apps/web/src/app/components/footer/Footer.tsx
+++ apps/web/src/app/components/footer/Footer.tsx
@@ -1,16 +1,34 @@
 import React from 'react';
 import { Logo } from '@/components';
+import { GitHubLogoIcon, LinkedInLogoIcon } from '@radix-ui/react-icons';
+import { GITHUB_URL, LINKEDIN_URL } from '@/lib/constants';
 
 export function Footer() {
   return (
-    <footer className="bg-gradient-to-r from-transparent via-indigo-50 dark:via-gray-800 mt-10">
-      <div className="flex justify-between max-w-screen-md py-5 mx-auto">
+    <footer className="bg-gray-800 mt-10">
+      <div className="flex items-center justify-around container py-5 mx-auto">
         <div className="flex flex-col items-start">
-          <Logo />
-          <p className="text-description">2022-present Elton Okawa</p>
+          <p className="text-indigo-300 font-semibold">Elton Okawa</p>
+          <p className="text-white">Full Stack Developer</p>
         </div>
-        {/* TODO add links */}
-        <div />
+        <div className="flex gap-3">
+          <a
+            href={LINKEDIN_URL}
+            target="_blank"
+            rel="noreferrer"
+            className="text-white hover:text-accent"
+          >
+            <LinkedInLogoIcon width={24} height={24} />
+          </a>
+          <a
+            href={GITHUB_URL}
+            target="_blank"
+            rel="noreferrer"
+            className="text-white hover:text-accent"
+          >
+            <GitHubLogoIcon width={24} height={24} />
+          </a>
+        </div>
       </div>
     </footer>
   );
